04627a2b9c285d8c5d283411be63c85f26eb7d62
Faez <faez@xmaagency.com>
2025-06-27 18:59:10 +0400

    Opening the proposal in new page

--- components/proposal/ProposalCard.tsx
+++ components/proposal/ProposalCard.tsx
@@ -248,6 +248,7 @@ const ProposalCard: React.FC<ProposalCardProps> = ({ proposal, onDelete }) => {
               href={`/proposal?token=${token}`}
               className="flex-1 bg-red-600 hover:bg-red-700 text-center px-3 py-2 rounded text-sm font-medium transition-colors flex items-center justify-center"
               prefetch={true}
+              target="_blank"
             >
               <Share2 size={14} className="mr-1" />
               View
